093f630dfa6e3cc79b6f9097dd7dffa68685bad3
Tanatip1996 <tanatipa58@email.nu.ac.th>
2018-01-23 21:38:47 +0700

    uplode

--- udp/client.py
+++ udp/client.py
@@ -1,18 +1,18 @@
 import socket
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 501
+UDP_PORT = 5015
 MESSAGE = input("Enter a number: ")
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print "UDP target IP : ", UDP_IP
+print "UDP target port : ", UDP_PORT
+
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.sendto(str(MESSAGE), (UDP_IP, UDP_PORT))
 
 data, addr = sock.recvfrom(1024)
-print 'Server response : ' + data
+print 'Result to factorial : ' + data
 
 
--- udp/server.py
+++ udp/server.py
@@ -2,7 +2,7 @@ import socket
 import math
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 501
+UDP_PORT = 5015
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
@@ -11,7 +11,8 @@ while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print "received message:", data
     factorial = math.factorial(int(data))
-    print "factorial:", factorial
+    print "Factorial:", factorial
     sock.sendto(str(factorial),addr)
+    
     
 
