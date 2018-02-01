a6eceada020e6da7e16bc8b2ef3f2b7021d9fd47
tanatip1996 <tanatipa58@email.nu.ac.th>
2018-02-01 22:58:53 +0700

    lode

--- Homework2/birth.py
+++ Homework2/birth.py
@@ -14,11 +14,10 @@ def calculate_age(born):
 
 parser = reqparse.RequestParser()
 parser.add_argument('birthdate')
-parser.add_argument('image', type=FileStorage, location='files')
 
 class Home(Resource):
 	def get(self):
-		return {"message":"'./born' to calculate age or './image' to upload image "}
+		return {"message":"./image' to upload image "}
 
 class Birth(Resource):
 	def get(self):
@@ -29,20 +28,10 @@ class Birth(Resource):
 		datetime_object = datetime.strptime(birthdate, '%d-%m-%Y')
 		age = int(calculate_age(datetime_object))
 		return {"birthdate":birthdate,"age":age}
-class Image(Resource):
-	def get(self):
-		return {"message":"Plese sent 'image' (POST method) to me."}
-	def post(self):
-		args = parser.parse_args()
-		image = args['image']
-		image_name = image.filename
-		image.save(image_name)
-		return {"code":200,"desc":"Upload success"}
 		
-
 api.add_resource(Home,'/')
 api.add_resource(Birth,'/born')
-api.add_resource(Image,'/image')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=5000)
 
